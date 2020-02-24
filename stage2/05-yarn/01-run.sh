7106e6fdcabb0df707551546e93be37708ae5c67
Owen Trueblood <hi@owentrueblood.com>
2020-02-23 20:43:05 -0500

    Hack to avoid cert validation failing in chroot

--- stage2/05-yarn/01-run.sh
+++ stage2/05-yarn/01-run.sh
@@ -12,7 +12,7 @@ on_chroot << EOF
   echo "Installing Yarn..."
   cd ${PROJECT_DIR}
 
-  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+  curl --insecure -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 
   echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
   sudo apt-get update && sudo apt-get install yarn
